f0f75e76d83686c26ff640f8acaca043f5f40a93
sireddyNaveen <49054832+sireddyNaveen@users.noreply.github.com>
2019-05-21 10:25:25 +0530

    Update even.c

--- even.c
+++ even.c
@@ -3,7 +3,7 @@
 main()
 {
 	int i;
-	printf("enter the number to check wheather it is positive or negative or zero");
+	printf("enter the number to check wheather it is even or odd");
 	scanf("%d",&i);
 	if((i>0)&&(i%2==0))
 	printf("positive number and even");
