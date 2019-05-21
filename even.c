21a5c6a6316dfefda821830f1cc6752820a9bd4b
sireddyNaveen <49054832+sireddyNaveen@users.noreply.github.com>
2019-05-21 09:08:14 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	printf("enter the number");
+	scanf("%d",&i);
+	if(i%2==0)
+	printf("even number");
+	else 
+	printf("odd number");
+	getch();
+}
+
